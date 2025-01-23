b2681d98569599ece75b9f26ddd7d26e73c60f01
Олег Ермошкин <Malepsacale@gmail.com>
2025-01-23 23:35:44 +0300

    pravki

--- docker/init_pg_hba.sh
+++ docker/init_pg_hba.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "host all all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
+set -e
+
+# Путь к pg_hba.conf
+PG_HBA_FILE="/var/lib/postgresql/data/pg_hba.conf"
+
+# Заменяем строку для IPv4
+#sed -i "s/^host\s\+all\s\+all\s\+127\.0\.0\.1\/32\s\+trust/host all all 0.0.0.0\/0 md5/" $PG_HBA_FILE
+echo "host    all             all             0.0.0.0/0               md5" >> $PG_HBA_FILE
+
+# Перезагружаем конфигурацию PostgreSQL
+pg_ctl reload -D /var/lib/postgresql/data
