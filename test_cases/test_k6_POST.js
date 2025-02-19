ecda63378fb391e3cef829270309b874f7c4fe1c
Олег Ермошкин <Malepsacale@gmail.com>
2025-02-19 18:18:13 +0300

    add tests jmeter

--- test_cases/test_k6_POST.js
+++ test_cases/test_k6_POST.js
@@ -6,15 +6,15 @@ export const options = {
         ramping_test: {
             executor: 'ramping-arrival-rate',
             startRate: 1, // Начальная скорость запросов
-            timeUnit: '60s', // Время для достижения целевой скорости
+            timeUnit: '1s', // Время для достижения целевой скорости
             preAllocatedVUs: 1, // Предварительно выделенные VUs
             stages: [
-                { target: 10, duration: '1m' }, // Поддерживаем 1 RPS в течение первой минуты
-                { target: 20, duration: '1m' }, // Увеличиваем до 2 RPS за одну минуту
-                { target: 20, duration: '2m' }, // Поддерживаем 2 RPS в течение двух минут
-                { target: 4, duration: '1m' }, // Увеличиваем до 4 RPS за одну минуту
-                { target: 4, duration: '2m' }, // Поддерживаем 4 RPS в течение двух минут
-                { target: 0, duration: '30s' }, // Останавливаем нагрузку за полминуты
+                { target: 2, duration: '1m' }, // Поддерживаем 1 RPS в течение первой минуты
+                { target: 2, duration: '2m' }, // Увеличиваем до 2 RPS за одну минуту
+                { target: 4, duration: '1m' }, // Поддерживаем 2 RPS в течение двух минут
+                { target: 4, duration: '2m' }, // Увеличиваем до 4 RPS за одну минуту
+                // { target: 4, duration: '2m' }, // Поддерживаем 4 RPS в течение двух минут
+                // { target: 0, duration: '30s' }, // Останавливаем нагрузку за полминуты
             ],
             // stage_2: {
             //     executor: 'constant-arrival-rate',
@@ -55,12 +55,12 @@ export default function () {
 
     // Отправляем POST-запрос
     const response = http.post(url, payload, params);
-
-    // Проверяем, что ответ успешный (код 200)
-    check(response, {
-        'is status 200': (r) => r.status === 200,
-        'response time < 200ms': (r) => r.timings.duration < 200,
-    });
+    //
+    // // Проверяем, что ответ успешный (код 200)
+    // check(response, {
+    //     'is status 200': (r) => r.status === 200,
+    //     'response time < 200ms': (r) => r.timings.duration < 200,
+    // });
 
     // Выводим ответ в консоль
     console.log('Response body: ' + response.body);
